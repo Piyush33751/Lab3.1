8dd3720d603b086a75fbc4f5fad14e46abe473f9
piyush <PIYUSH72.24@ichat.sp.edu.sg>
2025-05-11 22:20:54 +0800

    Hi

---
+++ Test_Lab2.py
@@ -0,0 +1,14 @@
+import Lab2.excersie2 as temp
+
+
+#def test_median_temp():
+    
+def test_bubble_sort_ascending():
+    print("Running test for median...")
+    My_list = [1, 2, 3, 4, 5]
+    x = 5
+    medi = temp.median_temp(My_list, x)
+    assert(medi==3)
+
+
+
